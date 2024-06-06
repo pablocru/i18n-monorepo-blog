029f96ecbe83336c53fb54d40a4711fa18f2599f
pablocru <pablocru@outlook.com>
2024-06-06 21:14:10 +0200

    refactor: Export the props of the components to take advantage of interface extension functionality and avoid code repetition

--- packages/components/head/index.ts
+++ packages/components/head/index.ts
@@ -1,6 +1,6 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import Head from './src/Head.astro';
+import Head, { type Props as HeadProps } from './src/Head.astro';
 
-export default Head;
+export { Head, type HeadProps };

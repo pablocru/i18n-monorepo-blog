d95e8c5b45473baced728aa153cda7f4bf9fd079
pablocru <pablocru@outlook.com>
2024-06-06 22:49:58 +0200

    chore: Create a component to manage HTML tags related to SEO

---
+++ packages/components/seo-tags/index.ts
@@ -0,0 +1,6 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+import SeoTags, { type Props as SeoTagsProps } from './src/SeoTags.astro';
+
+export { SeoTags, type SeoTagsProps };

5fce3494adc47d16586dafe7d9d179e2b6aa1b1f
pablocru <pablocru@outlook.com>
2024-06-08 21:00:54 +0200

    refactor: Merge head component into base layout

--- packages/components/head/index.ts
+++
@@ -1,6 +0,0 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
-
-import Head, { type Props as HeadProps } from './src/Head.astro';
-
-export { Head, type HeadProps };
